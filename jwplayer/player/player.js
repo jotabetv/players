c484e17ba783e4959ae13797f1e7392589100fd7
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-03-20 16:03:05 -0300

    Add files via upload

---
+++ jwplayer/player/player.js
@@ -0,0 +1,13 @@
+var video = document.getElementById('player');
+
+const player = jwplayer('player').setup({image: jw.image,mute: false,volume: 25,autostart: jw.auto,repeat: false,abouttext: jw.text,aboutlink: jw.link,skin: {active: jw.color
+},logo: {file: jw.logo,hide: true,link: jw.link,margin: '15',position: jw.lposi
+},sources: [{file: jw.file,type: 'video/mp4'
+}],})
+var url = window.location.href;
+url = url.split('?source=');
+url = url[1];
+alert(url);
+sources = { file: url };
+jwplayer().load(sources);
+jwplayer().play(true);
