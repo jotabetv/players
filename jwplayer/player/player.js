742f2199e2651e519f66fe0d1ccdcf50f4624089
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-03-25 10:15:17 -0300

    Update player.js

--- jwplayer/player/player.js
+++ jwplayer/player/player.js
@@ -1,22 +1,34 @@
 var jw = document.getElementById('player');
 
-var jw = {"file":"'",
-          "image":null,
-          "color":"0008ff",
-          "link":"https://tv0800.zip",
-          "logo":"",
-          "auto":"true",
-          "text":"JBF-TV",
-          "lposi":"bottom-left",
-          "flash":"https://tv0800.zip/wp-content/themes/dooplay/assets/jwplayer/jwplayer.flash.swf"}
-const player = jwplayer('player').setup({image: jw.image,mute: false,volume: 85,autostart: jw.auto,repeat: false,abouttext: jw.text,aboutlink: jw.link,skin: {active: jw.color
-},logo: {file: jw.logo,hide: true,link: jw.link,margin: '15',position: jw.lposi
-},sources: [{file: jw.file,type: 'video/mp4'
-}],})
+var playerInstance = jwplayer("player");
+playerInstance.setup({
+ sources: [{
+        file: "https://jbftv.m3u8",
+        image: "http://assets-jp.jwpsrv.com/thumbs/3XnJSIm4-640.jpg",
+        title: "Sintel Trailer",
+        color: "0008ff",
+        description: "Sintel is a fantasy CGI movie from the Blender Open Movie Project."
+}],
+    autostart: true,
+    mute: false,
+    controls: true,
+    aspectratio: "16:9",
+    stretching: "exactfit",      //uniform|fill|exactfit|bestfit|none
+    primary:"flash",
+    repeat: true,
+    height: '480',
+    width: '840',
+      listbar: {
+        position: "right",
+        size: 280
+      }
+          });
+
 var url = window.location.href;
 url = url.split('?source=');
-url = url[1];
-//alert(url);
+url = url[1]
+alert(url);
 sources = { file: url };
 jwplayer().load(sources);
 jwplayer().play(true);
+
