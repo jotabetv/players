7aadd3e35cdb2a944adbadadd6a847e25df89084
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-03-25 08:47:54 -0300

    Update player.js

--- jwplayer/player/player.js
+++ jwplayer/player/player.js
@@ -10,7 +10,7 @@ var jw = {"file":"'",
           "text":"JBF-TV",
           "lposi":"bottom-left",
           "flash":"https://tv0800.zip/wp-content/themes/dooplay/assets/jwplayer/jwplayer.flash.swf"}
-const player = jwplayer('player').setup({image: jw.image,mute: false,volume: 85,autostart: jw.auto,repeat: false,abouttext: jw.text,aboutlink: jw.link,skin: {active: jw.color
+const player = jwplayer('player').setup({image: jw.image,mute: false,volume: 85,autostart: true,repeat: false,abouttext: jw.text,aboutlink: jw.link,skin: {active: jw.color
 },logo: {file: jw.logo,hide: true,link: jw.link,margin: '15',position: jw.lposi
 },sources: [{file: jw.file,type: 'video/mp4'
 }],})
