4360bb1ee437902ee8f1a705b51eac508d15a0c1
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-03-26 08:10:09 -0300

    Add files via upload

---
+++ bmplayer/player/player.js
@@ -0,0 +1,24 @@
+var video = document.getElementById('player');
+
+var config = {
+        dataProvider: {
+            source: [
+                { url: "https://stream.live.novotempo.com/tv/smil:tvnovotempo.smil/playlist.m3u8" }
+            ]
+        }
+    };
+
+    /** find player container in html DOM tree. */
+    var element = document.getElementById("player");
+
+    /** create player */
+    var player = window.bradmax.player.create(element, config);
+var newurl = window.location.href;
+newurl = newurl.split('?source=');
+newurl = newurl[1];
+alert(newurl);
+source = { url: newurl };
+bradmax.player().load(source);
+bradmax.player().play(true);
+
+
