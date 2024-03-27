c6ae8290d42ccbb0d3cbc6643e9fda360021ada8
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-03-27 10:26:20 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -20,7 +20,14 @@ var config = {
 newurl = newurl.split('?src=');
 newurl = newurl[1];
 alert(newurl);
-source = { url : newurl };
+var config = {      
+        dataProvider: {
+            source: [
+                { url: newurl }
+            ]
+        }
+    };
+ var player = window.bradmax.player.create(element, config);
 //alert(newurl);
 // Aqui vai o codigo que vai rodar antes da pagina ser carregada...
 }
