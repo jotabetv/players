bc6fb2ff948b39a36068e0a98cf934f8bba64a17
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-03-27 14:17:39 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -26,7 +26,7 @@ var config = {
      var newurl = window.location.href;
 newurl = newurl.split('?src=');
 newurl = newurl[1];
-source = { url: alert(newurl };
+source = { url: alert(newurl) };
 //alert(newurl);      
 var element = document.getElementById("player");
  var player = window.bradmax.player.create(element, config);
