a05d41c9e207e3104efbb0604caa36b9308d8dc4
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-03 16:47:23 -0300

    Add files via upload

---
+++ jwplaylist/player.js
@@ -0,0 +1,66 @@
+/*
+    Copyright (c) 2024 Pierleeb
+    SPDX-License-Identifier: MIT
+*/
+var jw = document.getElementById('player');
+var playerInstance = jwplayer("player");
+playerInstance.setup({
+playlist: [{
+        file: "https://stream.live.novotempo.com/tv/smil:tvnovotempo.smil/playlist.m3u8",
+        image: "https://elielmedeiros.files.wordpress.com/2014/04/tv-novo-tempo-ao-vivo-small.jpg",
+        title: "TV Novo Tempo",
+        description: "O Canal da Esperança."
+}],
+    autostart: false,
+    mute: false,
+    controls: true,
+    aspectratio: "16:9",
+    stretching: "exactfit",      //uniform|fill|exactfit|bestfit|none
+    type: "hls",
+    primary:"flash",
+    repeat: true,
+    height: '480',
+    width: '840',
+      listbar: {
+        position: "right",
+        size: 280
+      }
+          });
+
+function setLink() {
+    let m3u8Player = document.getElementById("m3u8Player")
+    let Input = document.getElementById("Input")
+    let inputText = input.value  
+    let Input1 = document.getElementById("Input1")
+    let input1Text = input1.value
+    let Input2 = document.getElementById("Input2")
+    let input2Text = input2.value
+    
+   var playlist = jwplayer().getPlaylist();
+   var newItem = {
+        file: input.value,
+        image: input1.value,
+        title: input2.value
+    };
+    
+    if(jwplayer().getState() != "PLAYING"){
+        playlist.push(newItem);
+        jwplayer().load(playlist);
+    } else {
+        playlist.push(newItem);
+        var curpos = jwplayer().getPosition();
+        jwplayer().onPlaylist(function(){
+            jwplayer().seek(curpos);
+        });
+        jwplayer().load(playlist).onPlaylist(function(){
+            jwplayer().seek(curpos);
+        });
+    }
+alert('Canal ' + input2.value + ' inserido com sucesso!');
+}
+
+function next() {
+let player = document.getElementById("player")
+jwplayer().playlistNext();
+jwplayer().play(true);
+}
