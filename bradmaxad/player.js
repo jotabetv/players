56d5217b774a4771a2c001d5c1c673848e0b065f
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-13 21:12:56 -0300

    Update player.js

--- bradmaxad/player.js
+++ bradmaxad/player.js
@@ -10,6 +10,9 @@ var input = document.form.input;
         input.focus();
         return false;
 }else {
+	    window.onload = function() {
+  // Seu código JavaScript aqui
+
 let m3u8Player = document.getElementById("m3u8Player");
 let Input = document.getElementById("Input");
 let inputText = input.value;
@@ -19,11 +22,11 @@ let source = [{ url: n }];
 alert(n);
 
 	//var bradmaxPlayerConfig = {dataProvider:{source:[{url:"https://stmv1.srvstm.com/sistema7933/sistema7933/playlist.m3u8"}]},autoplay:true};
-	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};}
+	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
 	var element = document.getElementById("m3u8Player");
 	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
-
-
+}
+};
 
 
 }
