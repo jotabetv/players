1276e8b77d2b55322092e83b3db401546507661b
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-13 21:26:44 -0300

    Update player.js

--- bradmaxad/player.js
+++ bradmaxad/player.js
@@ -8,6 +8,7 @@ var input = document.form.input;
     if (input.value == ""){
 	window.alert("Url do vídeo ou áudio é obrigatório!");
         input.focus();
+	    window.location.reload(false);
         return false;
 }else {
 
