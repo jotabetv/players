8789ca5d7a141cdf3a585b64200ba9087205c53c
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-13 22:50:57 -0300

    Update player.js

--- bradmaxad/player.js
+++ bradmaxad/player.js
@@ -30,16 +30,7 @@ alert(n);
 
 }
 function play() {
-	  let m3u8Player = document.getElementById("m3u8Player");
-let Input = document.getElementById("Input");
-let inputText = input.value;
-let url = input.value;
-let n = input.value;
-let source = [{ url: n }];          
-alert(n);
+	window.location.reload(setLink)
+	  
 
-	//var bradmaxPlayerConfig = {dataProvider:{source:[{url:"https://stmv1.srvstm.com/sistema7933/sistema7933/playlist.m3u8"}]},autoplay:true};
-	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
-	var element = document.getElementById("m3u8Player");
-	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
 }
