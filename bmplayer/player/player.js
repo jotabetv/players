75bc7041adbd3773c1a1078c0ff8c13e4ed1c21b
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-14 14:50:41 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -10,7 +10,12 @@ alert(n);
 	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
 	var element = document.getElementById("m3u8Player");
 	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
-//window.onload = function() {
-  // Seu código JavaScript aqui
-//};
+if(!window.player) {
+				window.player = player;
+			}
+		}
+		if(window.bradmax && window.bradmax.player) {
+			setLink();
+		} else {
+			window.addEventListener('load', setLink);
 }
