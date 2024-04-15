97bcb3d152fe5643233222938bf8c3947e60477b
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-15 14:44:22 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -1,37 +1,37 @@
 //function setLink() {	
-	document.onload(funcaoPaginaCarregada(m3u8Player))
-function funcaoPaginaCarregada() {
-var m3u8Player = document.getElementById("m3u8Player");
-var url = window.location.href;
-url = url.split('?source=');
-url = n;
-alert(n);
+	//document.onload(funcaoPaginaCarregada(m3u8Player))
+//function funcaoPaginaCarregada() {
+//var m3u8Player = document.getElementById("m3u8Player");
+//var url = window.location.href;
+//url = url.split('?source=');
+//url = n;
+//alert(n);
 //var source = [{ url: n }];          
 
     Copyright (c) 2024 Pierleeb
     SPDX-License-Identifier: MIT
 */
-function refresh(){
-var input = document.form.input;
-var inputText = input.value;
-    if (input.value == ""){
-	window.alert("Url do vídeo ou áudio é obrigatório!");
-        return false;
-}else {
-alert('Canal ' + inputText + ' inserido com sucesso!');
-window.location.reload();
-	}
-}
+//function refresh(){
+//var input = document.form.input;
+//var inputText = input.value;
+  //  if (input.value == ""){
+	//window.alert("Url do vídeo ou áudio é obrigatório!");
+       // return false;
+//}else {
+//alert('Canal ' + inputText + ' inserido com sucesso!');
+//window.location.reload();
+	//}
+//}
 
 function setLink() {	
-var input = document.form.input;
-var inputText = input.value;
-    if (input.value == ""){
-        input.focus();
-        return false;
-}else {
-let m3u8Player = document.getElementById("m3u8Player");
-let Input = document.getElementById("Input");
+//var input = document.form.input;
+//var inputText = input.value;
+   // if (input.value == ""){
+       // input.focus();
+       // return false;
+//}else {
+var m3u8Player = document.getElementById("m3u8Player");
+//let Input = document.getElementById("Input");
 var url = window.location.href;
 url = url.split('?source=');
 url = n;
@@ -40,7 +40,7 @@ alert(n);
 	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
 	var element = document.getElementById("m3u8Player");
 	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
-}
+}//
 // Back compability.
 	if(!window.player) {
 	window.player = player;
