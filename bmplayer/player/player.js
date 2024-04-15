51b71165147cdbe5e83d07d2de863c8fc3103c2a
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-15 14:48:06 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -36,8 +36,8 @@ var url = window.location.href;
 url = url.split('?source=');
 url = n;
 alert(n);
-	
-	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
+	var bradmaxPlayerConfig = {dataProvider:{source:[{url:"https://stmv1.srvstm.com/sistema7933/sistema7933/playlist.m3u8"}]},autoplay:true};
+	//var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
 	var element = document.getElementById("m3u8Player");
 	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
 }//
