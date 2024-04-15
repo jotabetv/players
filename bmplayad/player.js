3043b600be4a8d4c6831dc5bd93ada07fc31d666
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-15 14:20:49 -0300

    Update player.js

--- bmplayad/player.js
+++ bmplayad/player.js
@@ -4,17 +4,11 @@
 */
 function refresh(){
 var input = document.form.input;
-    var inputText = input.value;
+var inputText = input.value;
     if (input.value == ""){
 	window.alert("Url do vídeo ou áudio é obrigatório!");
         return false;
 }else {
-//let m3u8Player = document.getElementById("m3u8Player");
-//let Input = document.getElementById("Input");
-//let inputText = input.value;
-//let url = input.value;
-//let n = input.value;
-//let source = [{ url: n }]; 
 alert('Canal ' + inputText + ' inserido com sucesso!');
 window.location.reload();
 	}
@@ -22,20 +16,16 @@ window.location.reload();
 
 function setLink() {	
 var input = document.form.input;
-    var inputText = input.value;
+var inputText = input.value;
     if (input.value == ""){
-	//window.alert("Url do vídeo ou áudio é obrigatório!");
         input.focus();
         return false;
 }else {
 let m3u8Player = document.getElementById("m3u8Player");
 let Input = document.getElementById("Input");
 let inputText = input.value;
-//let url = input.value;
 let n = input.value;
-//let source = [{ url: n }];          
-//alert(n);
-	//var bradmaxPlayerConfig = {dataProvider:{source:[{url:"https://stmv1.srvstm.com/sistema7933/sistema7933/playlist.m3u8"}]},autoplay:true};
+	
 	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
 	var element = document.getElementById("m3u8Player");
 	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
@@ -47,7 +37,7 @@ let n = input.value;
 	}
 	if(window.bradmax && window.bradmax.player) {
 		setLink();
-		input.value = "";
+		//input.value = "";
 	} else {
 window.addEventListener('load', setLink);
 }
