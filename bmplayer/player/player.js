41b472aff465e71223806f0a70c3e6419b66b356
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-15 14:38:02 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -8,19 +8,47 @@ url = n;
 alert(n);
 //var source = [{ url: n }];          
 
-
-	var bradmaxPlayerConfig = {dataProvider:{source:[{url:"https://stmv1.srvstm.com/sistema7933/sistema7933/playlist.m3u8"}]},autoplay:true};
-	//var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
-	var element = document.getElementById("m3u8Player");
-	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
-
-	//if(!window.player) {
-				//window.player = player;
-			//}
-		//}
-		//if(window.bradmax && window.bradmax.player) {
-			//setLink();
-		//} else {
-			//window.addEventListener('load', setLink);
+    Copyright (c) 2024 Pierleeb
+    SPDX-License-Identifier: MIT
+*/
+function refresh(){
+var input = document.form.input;
+var inputText = input.value;
+    if (input.value == ""){
+	window.alert("Url do vídeo ou áudio é obrigatório!");
+        return false;
+}else {
+alert('Canal ' + inputText + ' inserido com sucesso!');
+window.location.reload();
+	}
 }
 
+function setLink() {	
+var input = document.form.input;
+var inputText = input.value;
+    if (input.value == ""){
+        input.focus();
+        return false;
+}else {
+let m3u8Player = document.getElementById("m3u8Player");
+let Input = document.getElementById("Input");
+var url = window.location.href;
+url = url.split('?source=');
+url = n;
+alert(n);
+	
+	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
+	var element = document.getElementById("m3u8Player");
+	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
+}
+// Back compability.
+	if(!window.player) {
+	window.player = player;
+		}
+	}
+	if(window.bradmax && window.bradmax.player) {
+		setLink();
+		//input.value = "";
+	} else {
+window.addEventListener('load', setLink);
+}
