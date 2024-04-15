53ace623b88c7a500d20175ead13db5d196f1951
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-15 18:06:04 -0300

    Update player.js

--- jwplayer/player/player.js
+++ jwplayer/player/player.js
@@ -16,7 +16,7 @@ const player = jwplayer('player').setup({image: jw.image,mute: false,volume: 85,
 var url = window.location.href;
 url = url.split('?source=');
 url = url[1];
-//alert(url);
+alert(url);
 sources = { file: url };
 jwplayer().load(sources);
 jwplayer().play(true);
