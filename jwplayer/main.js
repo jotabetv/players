0cdc4411689644378551f53cc941509906af79fa
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-26 12:01:16 -0300

    Update main.js

--- jwplayer/main.js
+++ jwplayer/main.js
@@ -4,6 +4,15 @@
     JBF-TV Player - R.Santana/BA
 */
 
+function play-btn() {
+    var input = document.form.input;
+    var inputText = input.value;
+    if (input.value == ""){
+	window.alert("Url do vídeo ou áudio é obrigatório!");
+        input.focus();
+        return false;
+}else {
+
 $(window).on('load', function () {
     $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';
     $('#play-btn').on('click', function () {
@@ -11,3 +20,4 @@ $(window).on('load', function () {
         window.location.href = './player' + '?source=' + $('#m3u8-placeholder')[0].value;
     });
 });
+    }
