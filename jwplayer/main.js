3f5b110bfb8911835df243c3dcb5386eeee5c229
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-26 12:28:41 -0300

    Update main.js

--- jwplayer/main.js
+++ jwplayer/main.js
@@ -2,15 +2,18 @@
     Copyright (c) 2024 Pierleeb
     SPDX-License-Identifier: MIT
     JBF-TV Player - R.Santana/BA
-*/
+
 $(window).on('load', function () {
 if ($("#m3u8-placeholder").val() != '') {
     setflag = false;
     alert("cole o link aqui")
 } else {
-
+*/
 $(window).on('load', function () {
-    $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';
+  if ($('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';) {
+      setflag = false;
+    alert("cole o link aqui")
+}else {
     $('#play-btn').on('click', function () {
         localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
         window.location.href = './player' + '?source=' + $('#m3u8-placeholder')[0].value;
