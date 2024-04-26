54ad233f131e3d593374f7de3f1ad19e16ee6a48
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-26 12:31:32 -0300

    Update main.js

--- jwplayer/main.js
+++ jwplayer/main.js
@@ -18,7 +18,5 @@ $(window).on('load', function () {
         localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
         window.location.href = './player' + '?source=' + $('#m3u8-placeholder')[0].value;
     });
-});
-    
 }
 });
