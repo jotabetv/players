d5bab0d8c692c4dadd4a151a9b0247571ebc72b3
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-29 08:54:08 -0300

    Update main.js

--- jwplayer/main.js
+++ jwplayer/main.js
@@ -38,25 +38,20 @@ if($('input')[0].value == "") {
 	} else {
 */
 
-function play(){
-var input = document.form.input;
-var inputText = input.value;
-    if (input.value == ""){
-	window.alert("Url do vídeo ou áudio é obrigatório!");
-        return false;
-}else {
+
 	    
-}
-}
+
         $(window).on('load', function () {
-
-
+if($('input')[0].value == "") {
+		window.alert("Url do vídeo ou áudio é obrigatório!");
+	 window.location.href;
+}else {
     $('#input')[0].value = localStorage.getItem('m3u8-link') || '';
-    $('#btn').on('click', function () {
+    $('#play').on('click', function () {
         localStorage.setItem('m3u8-link', $('#input')[0].value);
         window.location.href = './player' + '?source=' + $('#input')[0].value;
-    });
-	
+    });	
+}
 });
     
 
