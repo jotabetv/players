76b75fcf1cc1c0300bce0ec5ac68c33d755a41ca
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-28 22:38:58 -0300

    Update main.js

--- jwplayer/main.js
+++ jwplayer/main.js
@@ -2,21 +2,34 @@
     Copyright (c) 2024 Pierleeb
     SPDX-License-Identifier: MIT
     JBF-TV Player - R.Santana/BA
-
-if (this.value == "Bike") {
-        $('#customer_two_wheel_input').show();
-        alert($(this).val())
-    }
-
-else  {
-        $('#customer_multiple_wheel_input').show();
-        alert($(this).val())
-    }
-*/
 $(window).on('load', function () {
     $('#input')[0].value = localStorage.getItem('m3u8-link') || '';
     $('#play-btn').on('click', function () {
         localStorage.setItem('m3u8-link', $('#input')[0].value);
         window.location.href = './player' + '?source=' + $('#input')[0].value;
     });
+});
+    
+
+$(".rdo_confirm").change(function(){  
+    if($(this).val() == "Y") {
+    $(this).addClass("active");
+            return false;
+      $("#btn_save").attr("disabled",false);
+    } else {
+        $("#btn_save").attr("disabled",true);
+    }
+});
+*/
+$(window).on('load', function () {
+    if($(this).val() == "") {
+    $(this).addClass("active");
+            return false;
+         } else {
+    $('#input')[0].value = localStorage.getItem('m3u8-link') || '';
+    $('#play-btn').on('click', function () {
+        localStorage.setItem('m3u8-link', $('#input')[0].value);
+        window.location.href = './player' + '?source=' + $('#input')[0].value;
+    });
+    }
 });
