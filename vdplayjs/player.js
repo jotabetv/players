2727604853351534666efce127c51ff20b960e0b
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-04-30 10:22:57 -0300

    Add files via upload

---
+++ vdplayjs/player.js
@@ -0,0 +1,60 @@
+/*
+    Copyright (c) 2024 Pierleeb
+    SPDX-License-Identifier: MIT
+    JBF-TV Player - R.Santana/BA
+*/
+
+function setLink() {
+var input = document.form.input;
+var inputText = input.value;
+    if (input.value == ""){
+	window.alert("Url do vídeo ou áudio é obrigatório!");
+	input.focus();
+        return false;
+   }else {
+window.alert('Canal ' + inputText + ' inserido com sucesso!');
+window.location.reload();
+	}
+}
+
+window.addEventListener('load', function() {
+var inputText = input.value;
+var n = inputText.slice(-4);
+
+if (n == "m3u8"){
+var videoPlayer = videojs('jb_player');
+if (input.value.trim() !== '') {
+        const source = {
+            src: input.value,
+            type: 'application/x-mpegURL'
+        };
+        videoPlayer.src(source);
+        videoPlayer.play();
+    }
+}
+
+if (n == ".mp4"){
+var videoPlayer = videojs('jb_player');
+if (input.value.trim() !== '') {
+        const source = {
+            src: input.value,
+            type: 'video/mp4'
+        };
+        videoPlayer.src(source);
+        videoPlayer.play();
+    }
+}
+
+if (n == ".mpd"){
+var videoPlayer = videojs('jb_player');
+if (input.value.trim() !== '') {
+        const source = {
+            src: input.value,
+            type: 'application/dash+xml'
+        };
+        videoPlayer.src(source);
+        videoPlayer.play();
+    }
+}
+
+});
