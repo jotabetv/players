0e18625fff8d650e3ee496d6f812b52d8e268885
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-05-02 15:12:03 -0300

    Update player.js

--- vdplayjs/player.js
+++ vdplayjs/player.js
@@ -2,77 +2,71 @@
     Copyright (c) 2024 Pierleeb
     SPDX-License-Identifier: MIT
     JBF-TV Player - R.Santana/BA
-    
-if (m == "yout"){
-var videoPlayer = videojs('jb_player');
-if (input.value.trim() !== '') {
-        const source = {
-            src: input.value,
-            type: 'video/youtube'
-        };
-        videoPlayer.src(source);
-        videoPlayer.play();
-    }
-}
 */
 
 function setLink() {
+var jb_player = document.getElementById("jb_player");
 var input = document.form.input;
 var inputText = input.value;
+var m = inputText.slice(-31,-27);
+var z = inputText.slice(-18,-13);
+var n = inputText.slice(-4);
+
     if (input.value == ""){
 	window.alert("Url do vídeo ou áudio é obrigatório!");
 	input.focus();
         return false;
    }else {
 window.alert('Canal ' + inputText + ' inserido com sucesso!');
-window.location.reload();
-	}
-}
-
-window.addEventListener('load', function() {
-var inputText = input.value;
-var vime = inputText.split("com/");
-var vime = vime[1];
-var tube = inputText.split("?v=");
-var tube = tube[1];
-var m = inputText.slice(-31,-27);
-var z = inputText.slice(-18,-13);
-var n = inputText.slice(-4);
+var videoPlayer = videojs('jb_player');
 
 if (n == ".mp4","webm",".mkv",".mp3",".ogg",".wav",".m4v",".ogv","aac"){
-var videoPlayer = videojs('jb_player');
-if (input.value.trim() !== '') {
-        const source = {
+var source = {
             src: input.value,
             type: 'video/mp4'
-        };
+             };
         videoPlayer.src(source);
-        videoPlayer.play();
-    }
-}
+	videoPlayer.play();
+	}
 
 if (n == "m3u8"){
-var videoPlayer = videojs('jb_player');
-if (input.value.trim() !== '') {
-        const source = {
+//if (input.value.trim() !== '') {
+var source = {
             src: input.value,
             type: 'application/x-mpegURL'
-        };
-        videoPlayer.src(source);
-        videoPlayer.play();
-    }
-}
+             };
+	videoPlayer.src(source);
+	videoPlayer.play();
+	}
 
 if (n == ".mpd"){
-var videoPlayer = videojs('jb_player');
-if (input.value.trim() !== '') {
-        const source = {
+var source = {
             src: input.value,
             type: 'application/dash+xml'
-        };
-        videoPlayer.src(source);
-        videoPlayer.play();
-    }
-}
+             };
+	videoPlayer.src(source);
+	videoPlayer.play();
+	}
 
-});
+if (m == "yout"){
+var source = {
+            src: input.value,
+            type: 'video/youtube',
+            provider: 'youtube'
+             };
+	videoPlayer.src(source);
+	videoPlayer.play();
+	}
+
+if (z == "vimeo"){
+var source = {
+            src: input.value,
+            type: 'video/vimeo',
+            provider: 'vimeo'
+	     };
+	videoPlayer.src(source);
+	videoPlayer.play();
+	}
+
+   }
+}
