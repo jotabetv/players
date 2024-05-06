2138481a79a87f1ba13a97b47c5e4e6bc4490eba
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-05-06 19:07:31 -0300

    Update player.js

--- bmplayad/player.js
+++ bmplayad/player.js
@@ -4,6 +4,10 @@
     JBF-TV Player - R.Santana/BA
 */
 
+function Carregar(){
+            document.getElementById("input").value = localStorage.getItem("input");
+        }
+
 function setLink(){
 var input = document.form.input;
 var inputText = input.value;
@@ -23,36 +27,9 @@ let JbPlayer = document.getElementById("JbPlayer");
 let Input = document.getElementById("Input");
 let inputText = input.value;
 let n = input.value;	
-//var input = document.form.input;
-//var inputText = input.value;
-   // if (input.value == ""){
-//window.alert("Url do vídeo ou áudio é obrigatório!");
-     //   input.focus();
-      //  return false;
-
-//}else {
-
-//window.alert('Canal ' + n + ' inserido com sucesso!');	
+window.alert('Canal ' + n + ' inserido com sucesso!');
+	
 	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
 	var element = document.getElementById("JbPlayer");
 	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
-
-//}
-// Back compability.
-	if(!window.player) {
-	window.player = player;
-		}
-	//}
-	if(window.bradmax && window.bradmax.player) {
-		//setLink();
-		//input.value = "";
-	} else {
-//window.addEventListener('load', setLink);
-}
-//window.addEventListener('load', function() {
 });
-
-
-function Carregar(){
-            document.getElementById("input").value = localStorage.getItem("input");
-        }
