8c65ba33448812a53b2f0e80b13307cf4fc0812a
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-05-06 17:54:36 -0300

    Update player.js

--- bmplayad/player.js
+++ bmplayad/player.js
@@ -2,81 +2,71 @@
     Copyright (c) 2024 Pierleeb
     SPDX-License-Identifier: MIT
     JBF-TV Player - R.Santana/BA
-    function setLink(){
-	var JbPlayer = document.getElementById("JbPlayer");
-	 var input = document.querySelector("#input").value;
-	 localStorage.setItem("arquivo1", input);
-	localStorage.setItem("arquivo1") = input.value;
-var input = document.form.input;
-var inputText = input.value;
-    if (input.value == ""){
-	window.alert("Url do vídeo ou áudio é obrigatório!");
-        return false;
-}else {
-window.alert('Canal ' + localStorage.setItem("arquivo1") + ' inserido com sucesso!');
-window.location.reload();
-	}
-}
-
-window.addEventListener('load', function() {
-var JbPlayer = document.getElementById("JbPlayer");
-	 var input = document.querySelector("#input").value;
-	 localStorage.setItem("arquivo1", input);
-var input = document.getElementById("input");
-var localStorage.setItem("arquivo1") = input.value;
-var n = input.value;
 */
+
 function refresh(){
 var input = document.form.input;
 var inputText = input.value;
+
     if (input.value == ""){
 	window.alert("Url do vídeo ou áudio é obrigatório!");
         return false;
+
 }else {
-	   // const input = document.querySelector("#input").value;
-	   // localStorage.setItem("arquivo1", input);
-  //console.log(input);
-window.alert('Canal ' + inputText + ' inserido com sucesso!');
-//window.location.reload();
+
+
+//alert('Canal ' + inputText + ' inserido com sucesso!');
+localStorage.setItem("input",document.getElementById("input").value);
+window.location.reload();
+
 	}
+
+
 }
 
-function setLink() {	
-	//var JbPlayer = document.getElementById("JbPlayer");
-	 //    var input = document.querySelector("#input").value;
-	 //  localStorage.setItem("arquivo1", input);
-//  console.log(input);
-var input = document.form.input;
-var inputText = input.value;
-    if (input.value == ""){
-	   // window.alert("Url do vídeo ou áudio é obrigatório!");
-	    
-        input.focus();
-        return false;
-}else {
-	    
+//function setLink() {
+
 
 //window.location.reload();
-//var JbPlayer = document.getElementById("JbPlayer");
-	    // var input = document.querySelector("#input").value;
- //localStorage.getItem("arquivo1") = input.value;
-let Input = document.getElementById("Input");    
-let n = input.value;
+	
 
-	//window.alert('Canal ' + localStorage.getItem("arquivo1") + ' inserido com sucesso!');
+
+//}
+window.addEventListener('load', function() {
+let JbPlayer = document.getElementById("JbPlayer");
+let Input = document.getElementById("Input");
+let inputText = input.value;
+let n = input.value;	
+//var input = document.form.input;
+//var inputText = input.value;
+   // if (input.value == ""){
+//window.alert("Url do vídeo ou áudio é obrigatório!");
+     //   input.focus();
+      //  return false;
+
+//}else {
+
+//window.alert('Canal ' + n + ' inserido com sucesso!');	
 	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
 	var element = document.getElementById("JbPlayer");
 	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
-}
+
+//}
 // Back compability.
 	if(!window.player) {
 	window.player = player;
 		}
-	}
+	//}
 	if(window.bradmax && window.bradmax.player) {
-		setLink();
+		//setLink();
 		//input.value = "";
 	} else {
 //window.addEventListener('load', setLink);
 }
+//window.addEventListener('load', function() {
+});
 
+
+function Carregar(){
+            document.getElementById("input").value = localStorage.getItem("input");
+        }
