b287dfa1c4c38b9c50d18ce3a177940f07ce0c25
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-05-05 22:59:26 -0300

    Update player.js

--- bmplayad/player.js
+++ bmplayad/player.js
@@ -3,48 +3,31 @@
     SPDX-License-Identifier: MIT
     JBF-TV Player - R.Santana/BA
 */
-
-function refresh(){
+function setLink(){
 var input = document.form.input;
 var inputText = input.value;
     if (input.value == ""){
 	window.alert("Url do vídeo ou áudio é obrigatório!");
         return false;
 }else {
-	   // const input = document.querySelector("#input").value;
-	   // localStorage.setItem("arquivo1", input);
-  //console.log(input);
 window.alert('Canal ' + inputText + ' inserido com sucesso!');
-//window.location.reload();
+window.location.reload();
 	}
 }
 
-function setLink() {	
-	//var JbPlayer = document.getElementById("JbPlayer");
-	 //    var input = document.querySelector("#input").value;
-	 //  localStorage.setItem("arquivo1", input);
-//  console.log(input);
-var input = document.form.input;
-var inputText = input.value;
-    if (input.value == ""){
-	   // window.alert("Url do vídeo ou áudio é obrigatório!");
-	    
-        input.focus();
-        return false;
-}else {
-	    
+window.addEventListener('load', function() {
+var jb_player = document.getElementById("jb_player");
+	 var input = document.querySelector("#input").value;
+	 localStorage.setItem("arquivo1", input);
+//var input = document.getElementById("input");
+var localStorage.setItem("arquivo1") = input.value;
+var n = input.value;
 
-//window.location.reload();
-//var JbPlayer = document.getElementById("JbPlayer");
-	    // var input = document.querySelector("#input").value;
- //localStorage.getItem("arquivo1") = input.value;
-let Input = document.getElementById("Input");    
-let n = input.value;
 	//window.alert('Canal ' + localStorage.getItem("arquivo1") + ' inserido com sucesso!');
 	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
 	var element = document.getElementById("JbPlayer");
 	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
-}
+//}
 // Back compability.
 	if(!window.player) {
 	window.player = player;
@@ -56,3 +39,4 @@ let n = input.value;
 	} else {
 //window.addEventListener('load', setLink);
 }
+});
