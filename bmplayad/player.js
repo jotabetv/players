b557fb786ad29d4b324c8d3aa444c52e15c3b52b
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-05-07 07:54:23 -0300

    Update player.js

--- bmplayad/player.js
+++ bmplayad/player.js
@@ -5,39 +5,38 @@
 */
 
 function Carregar(){
-            document.getElementById("input").value = localStorage.getItem("input");
-        }
+	document.getElementById("input").value = localStorage.getItem("input");
+	}
 
 function setLink(){
 var input = document.form.input;
 var inputText = input.value;
-    if (input.value == ""){
-	window.alert("Url do vídeo ou áudio é obrigatório!");
-	input.focus();
-        return false;
-}else {
-window.alert('Canal ' + inputText + ' inserido com sucesso!');
-localStorage.setItem("input",document.getElementById("input").value);
-window.location.reload();
+	if (input.value == ""){
+		window.alert("Url do vídeo ou áudio é obrigatório!");
+		input.focus();
+		return false;
+	}else {
+		window.alert('Canal ' + inputText + ' inserido com sucesso!');
+		localStorage.setItem("input",document.getElementById("input").value);
+		window.location.reload();
+		}
 	}
-}
 
 window.addEventListener('load', function() {
-let JbPlayer = document.getElementById("JbPlayer");
+let jb_player = document.getElementById("jb_player");
 let Input = document.getElementById("Input");
 let inputText = input.value;
 let n = input.value;	
-if (input.value == ""){
-	//window.alert("Url do vídeo ou áudio é obrigatório!");
-	input.focus();
-        return false;
-}else {
-	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
-	var element = document.getElementById("JbPlayer");
-	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
-localStorage.removeItem("input",document.getElementById("input").value);
-//window.alert('Canal ' + n + ' inserido com sucesso!');
-	localStorage.removeItem("n");
+	if (input.value == ""){
+		input.focus();
+		return false;
+	}else {
+var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
+var element = document.getElementById("jb_player");
+var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
+		
+	localStorage.removeItem("input",document.getElementById("input").value);
+	//localStorage.removeItem("n");
 	input.value = ''
-}
+	}
 });
