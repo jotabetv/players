d507992f61588665b4c99d9b5e38682a21d861ee
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-05-07 09:34:37 -0300

    Update player.js

--- jwplayad/player.js
+++ jwplayad/player.js
@@ -8,59 +8,59 @@ var jw = document.getElementById('JbPlayer');
 var playerInstance = jwplayer("JbPlayer");
 playerInstance.setup({
 playlist: [{
-        file: ".m3u8",
-        image: "https://imagem.natelinha.uol.com.br/original/5-emissoras-logo_eda9a984911a03a687f473e5711706ca959183e5.jpeg",
-        title: "JBF-TV",
-        description: "Todos os Canais em um só Lugar!"
-}],
-    autostart: false,
-    mute: false,
-    controls: true,
-    aspectratio: "16:9",
-    stretching: "exactfit",      //uniform|fill|exactfit|bestfit|none
-    type: "hls",
-    primary: "flash",
-    repeat: true,
-    height: '480',
-    width: '840',
-      listbar: {
-        position: "right",
-        size: 280
-      }
-          });
+	file: ".m3u8",
+	image: "https://imagem.natelinha.uol.com.br/original/5-emissoras-logo_eda9a984911a03a687f473e5711706ca959183e5.jpeg",
+	title: "JBF-TV",
+	description: "Todos os Canais em um só Lugar!"
+	}],
+	autostart: false,
+	mute: false,
+	controls: true,
+	aspectratio: "16:9",
+	stretching: "exactfit",      //uniform|fill|exactfit|bestfit|none
+	type: "hls",
+	primary: "flash",
+	repeat: true,
+	height: '480',
+	width: '840',
+	listbar: {
+		position: "right",
+		size: 280
+		}
+	});
 
 function setLink() {
-    var input = document.form.input;
-    var inputText = input.value;
-    if (input.value == ""){
-	window.alert("Url do vídeo ou áudio é obrigatório!");
-        input.focus();
-        return false;
-}else {
-    let JbPlayer = document.getElementById("JbPlayer")
-    let Input = document.getElementById("Input")
-    let inputText = input.value
-    let Input1 = document.getElementById("Input1")
-    let input1Text = input1.value
-    let Input2 = document.getElementById("Input2")
-    let input2Text = input2.value
+	var input = document.form.input;
+	var inputText = input.value;
+		if (input.value == ""){
+			window.alert("Url do vídeo ou áudio é obrigatório!");
+			input.focus();
+			return false;
+		}else {
+	let JbPlayer = document.getElementById("JbPlayer")
+	let Input = document.getElementById("Input")
+	let inputText = input.value
+	let Input1 = document.getElementById("Input1")
+	let input1Text = input1.value
+	let Input2 = document.getElementById("Input2")
+	let input2Text = input2.value
 
-    playlist = { file: input.value, 
-                image: input1.value,
-                title: input2.value };
-jwplayer().load(playlist);
-//jwplayer().play();
+	playlist = { file: input.value, 
+		image: input1.value,
+		title: input2.value };
+			jwplayer().load(playlist);
+			//jwplayer().play();
 
-window.alert('Canal ' + input2.value + ' inserido com sucesso!');
+		window.alert('Canal ' + input2.value + ' inserido com sucesso!');
 
-input.value = '',
-input1.value = '',
-input2.value = ''
-  }
-}
+		input.value = '',
+		input1.value = '',
+		input2.value = ''
+		}
+	}
 
 function next() {
-let JbPlayer = document.getElementById("JbPlayer")
-jwplayer().playlistNext();
-jwplayer().play(true);
-}
+	let JbPlayer = document.getElementById("JbPlayer")
+		jwplayer().playlistNext();
+		jwplayer().play(true);
+	}
