a6fbcf19944f12fd53b6de761253508cdf5ba269
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-05-07 08:11:57 -0300

    Update main.js

--- bmplayer/main.js
+++ bmplayer/main.js
@@ -5,17 +5,17 @@
 */
 
 function setLink(){
-var input = document.form.input;
-var inputText = input.value;
-    if (input.value == ""){
-	window.alert("Url do vídeo ou áudio é obrigatório!");
-	input.focus();
-        return false;
-   }else {
-let jb_player = document.getElementById("jb_player");
-let input = document.getElementById("input");
-let inputText = input.value;
-alert('Canal ' + inputText + ' inserido com sucesso!');
-window.location.href = './player' + '?source=' + inputText;	   
+	var input = document.form.input;
+	var inputText = input.value;
+	if (input.value == ""){
+		window.alert("Url do vídeo ou áudio é obrigatório!");
+		input.focus();
+        	return false;
+	}else {
+	let jb_player = document.getElementById("jb_player");
+	let input = document.getElementById("input");
+	let inputText = input.value;
+		window.alert('Canal ' + inputText + ' inserido com sucesso!');
+		window.location.href = './player' + '?source=' + inputText;	   
+		}
 	}
-}
