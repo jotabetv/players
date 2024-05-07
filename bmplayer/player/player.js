67f7d1c7e583046b7d264af31edd5ef5a71ad678
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-05-07 08:31:50 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -5,22 +5,24 @@
 */
 
 function setLink() {	
-var jb_player = document.getElementById("jb_player");
-var n = window.location.href;
-n = n.split('?source=');
-n = n[1];
-//alert(n);
-	
+	var jb_player = document.getElementById("jb_player");
+	var n = window.location.href;
+		n = n.split('?source=');
+		n = n[1];
+		//alert(n);
+
 	var bradmaxPlayerConfig = {dataProvider:{source:[{url:n}]},autoplay:true};
 	var element = document.getElementById("jb_player");
 	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
-// Back compability.
+
 	if(!window.player) {
-	window.player = player;
+		window.player = player;
 		}
-	
+	}
+
 	if(window.bradmax && window.bradmax.player) {
 		setLink();
-		//input.value = "";
-	}
-}
+		input.value = "";
+	} else {
+		window.addEventListener('load', setLink);
+		}
