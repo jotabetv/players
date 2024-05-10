c6d44011b32869d84b286bca396963c8e1eb986e
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-05-10 10:44:43 -0300

    Update player.js

--- jbframead/player.js
+++ jbframead/player.js
@@ -37,7 +37,7 @@ function play(){
 
 document.addEventListener("DOMContentLoaded", function(){
 
-   var els = document.body.querySelectorAll("a[href*='onclick']");
+   var els = document.body.querySelectorAll("a[href*='link']");
    
    for(var x=0; x<els.length; x++){
       
