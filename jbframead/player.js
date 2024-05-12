c54ddea22d374d0393a43efc7acc3f0ee006242a
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-05-12 02:58:30 +0000

    Update player.js

--- jbframead/player.js
+++ jbframead/player.js
@@ -38,40 +38,40 @@ function play(){
 
 // $(function(){
    
-   //var prods = [];  array para guardar os valores clicados
+   var prods = []; // array para guardar os valores clicados
    // evento "click" nos links
-  // $(".canais a").click(function(e){
+   $(".canais a").click(function(e){
       
-    //  e.preventDefault();  cancela o evento do link
-    //   var p = $(this).attr("rel"); pega o valor do atributo "rel" do link clicado
+      e.preventDefault(); // cancela o evento do link
+       var p = $(this).attr("rel"); //pega o valor do atributo "rel" do link clicado
       // verifica se o valor já existe na array.
       // se não existe, adiciona com "push"
       // se exite, remove "splice"
-    //  var indice = prods.indexOf(p);
-      //if(indice < 0) {
-  	   // prods.push(p);
-     // } else {
-  	 //   prods.splice(indice, 1);
-    //  }
+      var indice = prods.indexOf(p);
+      if(indice < 0) {
+  	    prods.push(p);
+      } else {
+  	    prods.splice(indice, 1);
+      }
       // converte a array em string com os valores separados por vírgula
       // e insere no input
-    //  $("#input").val(prods.join(","));
+      $("#input").val(prods.join(","));
       
-  // });
+   });
    
-//});
+});
 
-document.addEventListener("DOMContentLoaded", function(){
+//document.addEventListener("DOMContentLoaded", function(){
 
-   var els = document.body.querySelectorAll("a[href*='rel']");
+  // var els = document.body.querySelectorAll("a[href*='rel']");
    
-   for(var x=0; x<els.length; x++){
+  // for(var x=0; x<els.length; x++){
       
-      els[x].addEventListener("click", function(){
-         var a_txt = this.textContent,
-              inpt = document.body.querySelector("#input");
+     // els[x].addEventListener("click", function(){
+       //  var a_txt = this.textContent,
+           //   inpt = document.body.querySelector("#input");
               
-         inpt.value += " "+a_txt;
-      });
-   }
+        // inpt.value += " "+a_txt;
+   //   });
+//   }
 
