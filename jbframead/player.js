03598872e31563fc6c415c151b422e06a4b488d3
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-05-16 09:50:58 -0300

    Update player.js

--- jbframead/player.js
+++ jbframead/player.js
@@ -55,7 +55,7 @@ function Carregar(){
  
    var prods = []; // array para guardar os valores clicados
    // evento "click" nos links
-   $("#adicion a").click(function(e){
+   $(".adicion a").click(function(e){
       
       e.preventDefault(); // cancela o evento do link
        var p = $(this).attr("rel"); //pega o valor do atributo "rel" do link clicado
