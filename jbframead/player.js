9e73b403b4f99888856ff9f663d0b67c972ba9e4
jotabetv <163608789+jotabetv@users.noreply.github.com>
2024-06-16 21:04:24 -0300

    Update player.js

--- jbframead/player.js
+++ jbframead/player.js
@@ -45,7 +45,7 @@ function mudalink(){
 var as = document.getElementsByTagName('a');
 for( var i=0; i<as.length; i++ )
 {
-as[i].href = as[i].href.replace('/sinalpublico.co/','/xn----------------g34l3fkp7msh1cj3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn---22--11--33--99--75---------b25zjf3lta6mwf6a47dza94e.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja7ccgu2b4b0gd9deblk2jpa1b1e9zva7a0347s4da2797e8qri.xn--1ck2e1b/');
+as[i].href = as[i].href.replace('/sinalpublico.co/','/xn----------------g34l3fkp7msh1cj3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn---22--11--33--99--75---------b25zjf3lta6mwf6a47dza94e.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e8qri.xn--1ck2e1b/');
 }
 };
 
